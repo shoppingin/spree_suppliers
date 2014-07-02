0f5f1dbb4fa7ee9a168f602d74c76e649bb79203
Tsyren Ochirov <nsu1team@gmail.com>
2014-07-02 23:46:17 +0200

    Fix generator in flavour of new spree

--- lib/generators/spree_suppliers/install/install_generator.rb
+++ lib/generators/spree_suppliers/install/install_generator.rb
@@ -2,16 +2,16 @@ module SpreeSuppliers
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      include Rails::Generators::Migration
+      class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_suppliers\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_suppliers\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "\n//= require store/spree_suppliers\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "\n//= require admin/spree_suppliers\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_suppliers\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_suppliers\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_suppliers\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_suppliers\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
@@ -19,12 +19,12 @@ module SpreeSuppliers
       end
 
       def run_migrations
-         res = ask "Would you like to run the migrations now? [Y/n]"
-         if res == "" || res.downcase == "y"
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
